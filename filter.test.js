42b41457f840494c6693f7d8b57b458f5e4ae227
mleer1 <michel.leermakers@tuni.fi>
2023-12-08 18:08:33 +0200

    add tests for filter and isEmpty

---
+++ filter.test.js
@@ -0,0 +1,47 @@
+import filter from './src/filter'
+
+describe('filter function', () => {
+  it('filters elements based on the provided predicate', () => {
+    const users = [
+      { user: 'barney', active: true },
+      { user: 'fred', active: false },
+    ]
+
+    const filteredUsers = filter(users, ({ active }) => active)
+
+    expect(filteredUsers).toEqual([{ user: 'barney', active: true }])
+  })
+
+  it('handles an empty array correctly', () => {
+    const result = filter([], (item) => item)
+
+    expect(result).toEqual([])
+  })
+
+  it('handles a predicate that filters all elements', () => {
+    const array = [1, 2, 3, 4]
+    const predicate = () => false
+
+    const result = filter(array, predicate)
+
+    expect(result).toEqual([])
+  })
+
+  it('handles a predicate that includes all elements', () => {
+    const array = [1, 2, 3, 4]
+    const predicate = () => true
+
+    const result = filter(array, predicate)
+
+    expect(result).toEqual([1, 2, 3, 4])
+  })
+
+  it('does not modify the original array', () => {
+    const array = [1, 2, 3, 4]
+    const predicate = () => true
+
+    filter(array, predicate)
+
+    expect(array).toEqual([1, 2, 3, 4])
+  })
+})
---
+++ isEmpty.test.js
@@ -0,0 +1,63 @@
+import isEmpty from './src/isEmpty'
+
+describe('isEmpty function', () => {
+  it('returns true for null', () => {
+    expect(isEmpty(null)).toBe(true)
+  })
+
+  it('returns true for undefined', () => {
+    expect(isEmpty(undefined)).toBe(true)
+  })
+
+  it('returns true for an empty array', () => {
+    expect(isEmpty([])).toBe(true)
+  })
+
+  it('returns true for an empty string', () => {
+    expect(isEmpty('')).toBe(true)
+  })
+
+  it('returns true for an empty object', () => {
+    expect(isEmpty({})).toBe(true)
+  })
+
+  it('returns true for an empty Map', () => {
+    expect(isEmpty(new Map())).toBe(true)
+  })
+
+  it('returns true for an empty Set', () => {
+    expect(isEmpty(new Set())).toBe(true)
+  })
+
+  it('returns true for an empty function', () => {
+    expect(isEmpty(() => {})).toBe(true)
+  })
+
+  it('returns false for non-empty array', () => {
+    expect(isEmpty([1, 2, 3])).toBe(false)
+  })
+
+  it('returns false for non-empty string', () => {
+    expect(isEmpty('abc')).toBe(false)
+  })
+
+  it('returns false for non-empty object', () => {
+    expect(isEmpty({ a: 1 })).toBe(false)
+  })
+
+  it('returns false for non-empty Map', () => {
+    const nonEmptyMap = new Map([[1, 'one']])
+    expect(isEmpty(nonEmptyMap)).toBe(false)
+  })
+
+  it('returns false for non-empty Set', () => {
+    const nonEmptySet = new Set([1, 2, 3])
+    expect(isEmpty(nonEmptySet)).toBe(false)
+  })
+
+  it('returns false for function with properties', () => {
+    const funcWithProperties = () => {}
+    funcWithProperties.someProperty = 'property'
+    expect(isEmpty(funcWithProperties)).toBe(false)
+  })
+})
